29d40f3c029fb7170fc68dd808471c0d5e4176f1
Matthew Evans <mevans+git@wtfsystems.net>
2024-05-21 23:40:56 -0400

    sync

---
+++ _template/README.md
@@ -0,0 +1,9 @@
+#  script_templage
+
+your info here
+
+
+# Changelog
+
+## 1.0.0
+- Initial release
---
+++ _template/src/script-name.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/**
+ * @author Matthew Evans
+ * @module script_template
+ * @see README.md
+ * @copyright MIT see LICENSE.md
+ */
+
+console.log('Hello world!')
