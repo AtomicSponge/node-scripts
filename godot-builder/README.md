631e7c053a11b9c33b7cb9c650821f19032fa57d
Matthew Evans <mevans+git@wtfsystems.net>
2025-07-30 17:21:35 -0400

    sync

---
+++ godot-builder/README.md
@@ -0,0 +1,11 @@
+#  Godot Builder
+
+Script for building Godot projects.
+
+Usage:
+
+
+# Changelog
+
+## 1.0.0
+- Initial release
---
+++ godot-builder/src/godot-builder.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/**
+ * @author Matthew Evans
+ * @module godot-builder
+ * @see README.md
+ * @copyright MIT see LICENSE.md
+ */
+
+console.log('Hello world!')
